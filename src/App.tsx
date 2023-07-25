60be958ff91e3db3d612233ea7099d761a6b19ba
Rakic <lukarakic0@protonmail.com>
2023-07-25 12:20:22 +0200

    deleted sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
-import SignInPage from './pages/SignInPage';
-import ExplainPage from './pages/ExplainPage';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
+import { Routes, Route } from "react-router-dom";
+import ExplainPage from "./pages/ExplainPage";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -10,11 +9,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        {/* <Route path="/explain" element={<ExplainPage data={data} />} /> */}
-
         <Route path="/explain/:id" element={<ExplainPage />} />
-        {/*<Route path="/explain/code" element={<ExplainPage data={data} />} /> */}
       </Routes>
     </>
   );
